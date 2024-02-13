22097fb090b3128b16763d96ca93b06c15ecbe65
levidaniel96 <daniellevixd@gmail.com>
2024-02-14 00:51:57 +0200

    updated

--- DSPAudio Tool/main.py
+++ DSPAudio Tool/main.py
@@ -15,11 +15,7 @@ cs.store(name="create_data_config", node=create_data_config)
 @hydra.main(config_path = "conf", config_name = "config")
 
 def main(cfg: create_data_config): 
-    data=create_MyDataset(cfg.paths,cfg.params,cfg.flags)
-    data_len=data.len()
-    print(data_len)
-    for i in range(data_len):
-        data.getitem(i) 
+    create_MyDataset(cfg.paths,cfg.params,cfg.flags)
 if __name__ == '__main__':
     print('start')
     main()
