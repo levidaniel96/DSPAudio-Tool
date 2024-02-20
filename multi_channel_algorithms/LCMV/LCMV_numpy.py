c8f9681c545af81c3d647c22d2efe6ee5feb8997
levidaniel96 <daniellevixd@gmail.com>
2024-02-21 00:15:39 +0200

    updated LCMV

--- multi_channel_algorithms/LCMV/LCMV_numpy.py
+++ multi_channel_algorithms/LCMV/LCMV_numpy.py
@@ -3,51 +3,39 @@ import scipy.signal as ss
 
 
 
-def LCMV_RTFs(first_spk,second_spk,y,noise,RTFs,time_esti,wlen=4096,nfft=4096):#,estimate_RTF='no',RTFs=None):
-    #frame_count=Y_STFT_matrix.shape[2]
-    win = np.hamming(params.wlen)
-    overlap = int(params.wlen * 3 / 4)
-    params.NUP = int(nfft/2)+1
-    params.n_hop=params.wlen-overlap
-
+def LCMV_RTFs(y,RTFs,Qvv,params):
+    """
+    LCMV_RTFs: LCMV beamforming algorithm for multi-channel speech enhancement
+    Inputs:
+    y: noisy speech signal (n_samples, n_channels)
+    RTFs: Room Transfer Functions (n_frequencies, n_channels, n_sources)
+    Qvv: Noise covariance matrix (n_frequencies, n_channels, n_channels)
+    params: parameters of the algorithm
+    Outputs:
+    first_channel: enhanced signal for the first channel
+    second_channel: enhanced signal for the second channel
+    """
+    
     frame_count = 1 + (y.shape[0] - params.wlen ) // params.n_hop
 
-    Y_STFT_matrix=np.zeros([params.NUP,frame_count,M],dtype=complex)
-    First_spk_STFT=np.zeros([params.NUP,frame_count,M],dtype=complex)
-    Second_spk_STFT=np.zeros([params.NUP,frame_count,M],dtype=complex)
-    N_STFT=np.zeros([params.NUP,frame_count,M],dtype=complex)
+    Y_STFT_matrix=np.zeros([params.NUP,frame_count,params.M],dtype=complex)
 
-
-    for m in range(M):
-        Y_STFT_matrix[:,:,m]=ss.stft(y[:,m],fs, win , nperseg=params.wlen, noverlap=overlap, nfft=nfft,boundary=None,padded=False)[2] 
-        First_spk_STFT[:,:,m]=ss.stft(first_spk[:,m],fs, win , nperseg=params.wlen, noverlap=overlap, nfft=nfft,boundary=None,padded=False)[2] 
-        Second_spk_STFT[:,:,m]=ss.stft(second_spk[:,m],fs, win , nperseg=params.wlen, noverlap=overlap, nfft=nfft,boundary=None,padded=False)[2]
-        N_STFT[:,:,m]=ss.stft(noise[:,m],fs, win , nperseg=params.wlen, noverlap=overlap, nfft=nfft,boundary=None,padded=False)[2] 
+    for m in range(params.M):
+        Y_STFT_matrix[:,:,m]=ss.stft(y[:,m],params.fs, np.hamming(params.wlen) , nperseg=params.wlen, noverlap=params.overlap, nfft=params.NFFT,boundary=None,padded=False)[2] 
 
     output_y_stft = np.zeros([params.NUP,2,frame_count], dtype=complex)
-    output_first_stft = np.zeros([params.NUP,2,frame_count], dtype=complex)
-    output_second_stft = np.zeros([params.NUP,2,frame_count], dtype=complex)
-    output_n_stft = np.zeros([params.NUP,2,frame_count], dtype=complex)
     e=1e-6
-  
-    w=np.zeros([M,2,params.NUP],dtype=complex)
-
-    _,Qvv = creat_Qvv_Qzz(Y_STFT_matrix,time_esti,nfft)
-    
+    w=np.zeros([params.M,2,params.NUP],dtype=complex)
     for k in range(params.NUP):
         g = (RTFs[k,:,:].squeeze())
         b = (Qvv[k,:,:].squeeze())
-        inv_b = b + e*np.linalg.norm(b)*np.eye(M)
+        inv_b = b + e*np.linalg.norm(b)*np.eye(params.M)
         c = np.matmul(np.linalg.inv(inv_b),g)
         g_conj = np.conj(g).T
         inv_temp =  np.matmul(g_conj,c) + e*np.linalg.norm(np.matmul(g_conj,c))*np.eye(2)
         w[:,:,k] = (np.matmul(c , np.linalg.inv(inv_temp)))
-        
         output_y_stft[k,:,:] = np.matmul(np.conj(w[:,:,k].squeeze()).T,(Y_STFT_matrix[k,:,:].squeeze()).T)
-        output_first_stft[k,:,:] = np.matmul(np.conj(w[:,:,k].squeeze()).T,(First_spk_STFT[k,:,:].squeeze()).T)
-        output_second_stft[k,:,:] = np.matmul(np.conj(w[:,:,k].squeeze()).T,(Second_spk_STFT[k,:,:].squeeze()).T)
-        output_n_stft[k,:,:] = np.matmul(np.conj(w[:,:,k].squeeze()).T,(N_STFT[k,:,:].squeeze()).T)
-
-
+    first_channel=ss.istft(output_y_stft[:,0,:].squeeze(),params.fs,window=np.hamming(params.wlen), nperseg=params.wlen, noverlap=params.overlap, nfft=params.NFFT)[1]
+    second_channel=ss.istft(output_y_stft[:,1,:].squeeze(),params.fs,window=np.hamming(params.wlen), nperseg=params.wlen, noverlap=params.overlap, nfft=params.NFFT)[1]
         
-    return output_y_stft,output_first_stft,output_second_stft,output_n_stft
+    return first_channel, second_channel
